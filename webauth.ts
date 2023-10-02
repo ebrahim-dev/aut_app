b6975497a8d1397211c658bc385658b8ce2ebb6b
Ebrahim <127089626+AlSermani@users.noreply.github.com>
2023-10-02 15:14:44 +0200

    nog niet geregeld

---
+++ webauth.ts
@@ -0,0 +1,5 @@
+import * as webauthn from "@passwordless-id/webauthn";
+import { client } from "@passwordless-id/webauthn";
+import { server } from "@passwordless-id/webauthn";
+import { parsers } from "@passwordless-id/webauthn";
+import { utils } from "@passwordless-id/webauthn";
